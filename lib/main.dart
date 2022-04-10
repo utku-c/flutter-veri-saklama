8291955c2978db06e81b4d30b540a707e8d37d2a
utku-c <utkubilgin47@gmail.com>
2022-04-10 20:54:14 +0300

    Veri Saklama

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:basit_veri_saklama/preferences/shared_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SharedView(),
+    );
+  }
+}
